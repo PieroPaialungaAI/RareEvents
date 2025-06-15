11089bfebdb5b5e157380274feacf50ddc8c20af
Piero Paialunga <piero.paialunga@hotmail.com>
2025-06-15 14:58:10 -0400

    Building data loader

---
+++ constants.py
@@ -0,0 +1,2 @@
+DEFAULT_DATA_PATH = 'RawData/'
+DEFAULT_DATA_FILE = 'weather_data.csv'
---
+++ data.py
@@ -0,0 +1,14 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd
+from constants import * 
+
+class Data:
+    def __init__(self, data_path = DEFAULT_DATA_PATH, data_file = DEFAULT_DATA_FILE):
+        self.data_path = data_path
+        self.data_file = data_file
+        self.raw_data = self.load_data_path()
+
+    
+    def load_data_path(self):
+        return pd.read_csv(self.data_path+self.data_file)
